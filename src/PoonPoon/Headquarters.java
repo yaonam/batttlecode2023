5fddc2515f4434c2a2c7bd95b4a57fde3f2d91a3
Nawlej <churknam@gmail.com>
2023-01-12 20:20:10 -0800

    HQ class minor updates

--- src/PoonPoon/Headquarters.java
+++ src/PoonPoon/Headquarters.java
@@ -12,16 +12,7 @@ public class Headquarters extends Base{
         // Pick a direction to build in. Dependent on location of HQ. We split the map into quadrants. Build units towards the middle of the map. We can also set HQ to build carriers 
         // towards wells 
         WellInfo[] wellInfo = rc.senseNearbyWells();
-        if (wellInfo.length != 0) {
-            MapLocation wellLocation = wellInfo[0].getMapLocation();
-            build_Direction = rc.getLocation().directionTo(wellLocation);
-            initial_build_location = rc.getLocation().add(build_Direction).add(build_Direction).add(build_Direction);
-        } else {
-            starting_x_coord = rc.getLocation().x;
-            starting_y_coord = rc.getLocation().y;
-            build_Direction = initialBuildDIrection(rc);
-            initial_build_location = rc.getLocation().add(build_Direction).add(build_Direction).add(build_Direction);
-        }
+        setInitialBuildLocation(rc, wellInfo);
         
         // if (rc.canBuildAnchor(Anchor.STANDARD)) {
         //     // If we can build an anchor do it!
@@ -37,7 +28,20 @@ public class Headquarters extends Base{
         buildRobot(rc, RobotType.LAUNCHER);
     }
 
-    public Direction initialBuildDIrection (RobotController rc) {
+    public void setInitialBuildLocation (RobotController rc, WellInfo[] wellInfo) {
+        if (wellInfo.length != 0) {
+            MapLocation wellLocation = wellInfo[0].getMapLocation();
+            build_Direction = rc.getLocation().directionTo(wellLocation);
+            initial_build_location = rc.getLocation().add(build_Direction).add(build_Direction).add(build_Direction);
+        } else {
+            starting_x_coord = rc.getLocation().x;
+            starting_y_coord = rc.getLocation().y;
+            build_Direction = initialBuildDirection(rc);
+            initial_build_location = rc.getLocation().add(build_Direction).add(build_Direction).add(build_Direction);
+        }
+    }
+
+    public Direction initialBuildDirection (RobotController rc) {
         String quadrant = initialMapQuadrant(rc, starting_x_coord, starting_y_coord);
         return initialDirection(rc, quadrant);
     }
@@ -60,11 +64,9 @@ public class Headquarters extends Base{
     public void buildRobot(RobotController rc, RobotType robotType) throws GameActionException{
         if (rc.canBuildRobot(robotType, build_location)) {
             // Let's try to build a robot.
-            rc.setIndicatorString("Trying to build a " + robotType);
             System.out.println("Trying to build a " + robotType);
             if (rc.canBuildRobot(robotType, build_location)) {
                 rc.buildRobot(robotType, build_location);
-                // rc.writeSharedArray(carrierCountIndex, rc.readSharedArray(carrierCountIndex)+1);
             }
         } 
     }
